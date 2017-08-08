35527d46852a6a619e34f41ec1bb589f003ea2cf
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-08-08 14:47:33 +0100

    updated script with help comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,20 @@
+// socket.io
 const socket = io();
 
-//On page text
+//Adds text to on input and output page chat
 const outputYou = document.querySelector('.output-you');
 const outputBot = document.querySelector('.output-bot');
 
+// Web speech API
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; //including both prefixed and non-prefixed objects, because Chrome currently supports the API with prefixed properties.
 const recognition = new SpeechRecognition();
 
+// web speech API config
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1
 
+//Once button is pressed this will start the microphone function
 document.querySelector('button').addEventListener('click', () => {
   recognition.start();
 });
